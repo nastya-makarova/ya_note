83e1656c725ae20d0a48a0545be09e2dd8cf862b
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-08-09 01:20:26 +0400

    Added checking for non author for editing and deleting.

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -15,14 +15,16 @@ class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='John Snow')
+        cls.reader = User.objects.create(username='Ned Stark')
         cls.notes = Note.objects.create(
             title='Название заметки',
             text='Текст заметки',
             author=cls.author
         )
 
-    def test_pages_availability(self):
-        """Метод тестирует доступность для анонимных пользователей главной страницы,
+    def test_pages_availability_for_unauthorized_user(self):
+        """Метод тестирует доступность для анонимных пользователей 
+        главной страницы,
         cтраницы регистрации пользователей,
         страниц входа в учётную запись и выхода из неё.
         """
@@ -41,7 +43,7 @@ class TestRoutes(TestCase):
     def test_pages_availability_for_author(self):
         """Метод проверяет доступность автору страниц:
         страницы отдельной заметки, страницы списка заметок,
-        страниц удаления и редактирования заметки.
+        страницы создания заметки.
         """
         # Логиним пользователя в клиенте:
         self.client.force_login(self.author)
@@ -50,11 +52,9 @@ class TestRoutes(TestCase):
         # Каждый вложенный кортеж содержит два элемента:
         # имя пути и позиционные аргументы для функции reverse().
         urls = (
+            ('notes:add', None),
             ('notes:detail', (self.notes.slug,)),
             ('notes:list', None),
-            ('notes:edit', (self.notes.slug,)),
-            ('notes:delete', (self.notes.slug,)),
-
         )
 
         for name, args in urls:
@@ -62,3 +62,20 @@ class TestRoutes(TestCase):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_comment_edit_and_delete(self):
+        """Метод проверяет, что страницы редактирования и удаления
+        заметки доступны автору, но недоступны другому пользователю.
+        """
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND)
+        )
+
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('notes:edit', 'notes:delete'):
+                with self.subTest(name=name, user=user):
+                    url = reverse(name, args=(self.notes.slug,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
