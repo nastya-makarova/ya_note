cce951a83133962ce3254b464de58a9f0048ac0f
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-08-09 18:22:05 +0400

    Added test for checking ordering of notes on the list page.

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -28,9 +28,25 @@ class TestListPage(TestCase):
             for index in range(NOTES_COUNT)
         )
 
-    def test_notes_count(self):
+    def get_object_list(self):
         self.client.force_login(self.author)
         response = self.client.get(self.LIST_URL)
-        object_list = response.context['object_list']
+        return response.context['object_list']
+
+    def test_notes_count(self):
+        """Метод проверяет, что на странице список заметок
+        отображаются все заметки автора
+        """
+        object_list = self.get_object_list()
         notes_count = object_list.count()
         self.assertEqual(notes_count, NOTES_COUNT)
+
+    def test_notes_order(self):
+        """Метод проверяет, что заметки отсортированы от
+        самой первой к последней по id.
+        """
+        object_list = self.get_object_list()
+        # Получаем id  заметок в том порядке, как они выведены на странице.
+        id_list_on_page = [note.id for note in object_list]
+        sorted_id = sorted(id_list_on_page)
+        self.assertEqual(id_list_on_page, sorted_id)
